3f9b8870306f8513a63d7f43c86316f793badc19
sagarverma <intern.sagar.verma@firstcry.in>
2021-02-10 18:07:43 +0530

    Components Added and Routing Done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { CustomersComponent } from './customers/customers.component';
+import { OrdersComponent } from './orders/orders.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+   {path: 'Customers', component: CustomersComponent},
+   {path: 'Orders', component: OrdersComponent},
+   {path: 'About', component: AboutComponent},
+   {path: 'Login', component: LoginComponent},
+   { path: '', redirectTo: '/Customers', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { CustomersComponent } from './customers/customers.component';
+import { OrdersComponent } from './orders/orders.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CustomersComponent,
+    OrdersComponent,
+    AboutComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
